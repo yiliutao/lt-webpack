5865cdbf43199a48a6bcd227efbbc30ce99f6693
yiliutao <yiliutao@163.com>
2021-04-13 19:18:16 +0800

    测试模块link

--- src/page/app.tsx
+++ src/page/app.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
+import {getLocalItem, getSessionItem} from 'lt_hello_tao/dist/utils/util';
 
 interface IProps{}
 interface IState{}
 export default class App extends React.Component<IProps, IState>{
+    componentDidMount(){
+        getLocalItem();
+        getSessionItem();
+    }
     render(){
         return <div>hello LT</div>;
     }
